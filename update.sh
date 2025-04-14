d4dba91d887aeabaffb7023671d79c9ffdc43c9c
root <tanakamasayuki@gmail.com>
2025-04-14 15:58:59 +0900

    update

--- update.sh
+++ update.sh
@@ -15,13 +15,13 @@ cd ..
 # https://www.wch.cn/products/CH32V003.html
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32V003DS0_PDF.html
-curl -z CH32V003DS0.PDF -o CH32V003DS0.PDF https://www.wch.cn/downloads/file/408.html
+curl -z CH32V003DS0.PDF -o CH32V003DS0.PDF https://www.wch.cn/download/file?id=408
 # https://www.wch.cn/downloads/CH32V003RM_PDF.html
-curl -z CH32V003RM.PDF -o CH32V003RM.PDF https://www.wch.cn/downloads/file/411.html
+curl -z CH32V003RM.PDF -o CH32V003RM.PDF https://www.wch.cn/download/file?id=411
 cd ..
 
 # https://www.wch.cn/downloads/CH32V003EVT_ZIP.html
-curl -z CH32V003EVT.ZIP -o CH32V003EVT.ZIP https://www.wch.cn/downloads/file/409.html
+curl -z CH32V003EVT.ZIP -o CH32V003EVT.ZIP https://www.wch.cn/download/file?id=409
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
