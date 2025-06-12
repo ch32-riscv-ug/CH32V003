e240435a6436caf517d60c8a90e1b84ab33cf48a
root <tanakamasayuki@gmail.com>
2025-06-12 10:44:20 +0900

    update

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ git pull
 # https://www.wch-ic.com/products/CH32V003.html
 cd datasheet_en
 # https://www.wch-ic.com/downloads/CH32V003DS0_PDF.html
-curl -z CH32V003DS0.PDF -o CH32V003DS0.PDF https://www.wch-ic.com/downloads/file/359.html
+curl -z CH32V003DS0.PDF -o CH32V003DS0.PDF https://www.wch-ic.com/download/file?id=359
 # https://www.wch-ic.com/downloads/CH32V003RM_PDF.html
-curl -z CH32V003RM.PDF -o CH32V003RM.PDF https://www.wch-ic.com/downloads/file/358.html
+curl -z CH32V003RM.PDF -o CH32V003RM.PDF https://www.wch-ic.com/download/file?id=358
 cd ..
 
 # https://www.wch.cn/products/CH32V003.html
